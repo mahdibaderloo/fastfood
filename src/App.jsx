0928a3bbd3feece4f719fde64074a1f391535338
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-25 23:08:29 +0330

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Loading from "./components/Loading";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([{ path: "/" }]);
+const router = createBrowserRouter([{ path: "/", element: <Loading /> }]);
 
 function App() {
   return (
---
+++ src/components/Loading.jsx
@@ -0,0 +1,65 @@
+import pizza1 from "../data/images/loading-1.png";
+import pizza2 from "../data/images/loading-2.png";
+import pizza3 from "../data/images/loading-3.png";
+import pizza4 from "../data/images/loading-4.png";
+import pizza5 from "../data/images/loading-5.png";
+import pizza6 from "../data/images/loading-6.png";
+import pizza7 from "../data/images/loading-7.png";
+import pizza8 from "../data/images/loading-8.png";
+
+function Loading() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center relative bg-amber-300">
+      <img
+        src={pizza1}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "0.5s" }}
+      />
+      <img
+        src={pizza2}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "0.65s" }}
+      />
+      <img
+        src={pizza3}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "0.8s" }}
+      />
+      <img
+        src={pizza4}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "0.95s" }}
+      />
+      <img
+        src={pizza5}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "1.1s" }}
+      />
+      <img
+        src={pizza6}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "1.25s" }}
+      />
+      <img
+        src={pizza7}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "1.4s" }}
+      />
+      <img
+        src={pizza8}
+        alt="pizza-icon"
+        className="w-28 absolute animate-opacity"
+        style={{ animationDelay: "1.55s" }}
+      />
+    </div>
+  );
+}
+
+export default Loading;
