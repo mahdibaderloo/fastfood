267d2972114bde29b0a115d153019f1e2a74fcfd
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-25 23:28:10 +0330

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
+import Menu from "./pages/Menu";
+import Dashboard from "./pages/Dashboard";
+import Notifications from "./pages/Notifications";
+import Settings from "./pages/Settings";
+import Cart from "./pages/Cart";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    children: [
+      { path: "/", element: <Menu /> },
+      { path: "/dashboard", element: <Dashboard /> },
+      { path: "/notifications", element: <Notifications /> },
+      { path: "/settings", element: <Settings /> },
+      { path: "/cart", element: <Cart /> },
+      { path: "/notifications", element: <Notifications /> },
+    ],
   },
 ]);
 
