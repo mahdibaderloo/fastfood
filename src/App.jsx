3122b49f5c4972ae44cc34ab6e1a03da36018cba
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-25 23:27:54 +0330

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Loading from "./components/Loading";
 import AppLayout from "./components/AppLayout";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
-  { element: <AppLayout />, children: [{ path: "/" }] },
+  {
+    element: <AppLayout />,
+  },
 ]);
 
 function App() {
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function AppLayout() {
-  return <div></div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default AppLayout;
