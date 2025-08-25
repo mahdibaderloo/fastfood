344c32c981ec0e174e99a0c56b43a28bd1099b62
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-26 00:19:52 +0330

    navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,55 @@
-import { GoPerson } from "react-icons/go";
-import { IoIosNotificationsOutline } from "react-icons/io";
+import { BiSolidNotification } from "react-icons/bi";
+import { GoPerson, GoPersonFill } from "react-icons/go";
 import {
+  IoCart,
   IoCartOutline,
   IoFastFoodOutline,
+  IoFastFoodSharp,
   IoSettingsOutline,
+  IoSettingsSharp,
 } from "react-icons/io5";
+import { TbNotification } from "react-icons/tb";
 import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="flex justify-between rounded-xl bg-white fixed left-0 right-0 bottom-0 p-4 m-2">
       <NavLink to="/dashboard">
-        <GoPerson size={30} />
+        {({ isActive }) =>
+          isActive ? <GoPersonFill size={34} /> : <GoPerson size={34} />
+        }
       </NavLink>
       <NavLink to="/cart">
-        <IoCartOutline size={30} />
+        {({ isActive }) =>
+          isActive ? <IoCart size={34} /> : <IoCartOutline size={34} />
+        }
       </NavLink>
       <NavLink to="/">
-        <IoFastFoodOutline size={30} />
+        {({ isActive }) =>
+          isActive ? (
+            <IoFastFoodSharp size={34} />
+          ) : (
+            <IoFastFoodOutline size={34} />
+          )
+        }
       </NavLink>
       <NavLink to="/notifications">
-        <IoIosNotificationsOutline size={30} />
+        {({ isActive }) =>
+          isActive ? (
+            <BiSolidNotification size={34} />
+          ) : (
+            <TbNotification size={34} />
+          )
+        }
       </NavLink>
       <NavLink to="/settings">
-        <IoSettingsOutline size={30} />
+        {({ isActive }) =>
+          isActive ? (
+            <IoSettingsSharp size={34} />
+          ) : (
+            <IoSettingsOutline size={34} />
+          )
+        }
       </NavLink>
     </nav>
   );
