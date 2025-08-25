77e4d95b0a22d5a012ca63f4eeec08f41f2f799b
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-25 23:14:39 +0330

    add AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Loading from "./components/Loading";
+import AppLayout from "./components/AppLayout";
 
 const queryClient = new QueryClient();
 
-const router = createBrowserRouter([{ path: "/", element: <Loading /> }]);
+const router = createBrowserRouter([
+  { element: <AppLayout />, children: [{ path: "/" }] },
+]);
 
 function App() {
   return (
