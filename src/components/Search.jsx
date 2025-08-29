aa75a663917b48038f012d20b5e6a5cc5ce787f1
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-29 14:47:24 +0330

    Search styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,14 @@ import { BiSearchAlt } from "react-icons/bi";
 function Search() {
   return (
     <div className="bg-neutral-50 flex items-center p-2 rounded-lg">
+      <input
+        type="text"
+        placeholder="Search for..."
+        className="w-full h-full outline-none text-neutral-900"
+      />
       <p>
         <BiSearchAlt size={40} color="#2c2c2c" />
       </p>
-      <input
-        type="text"
-        className="w-full h-full outline-none text-neutral-900"
-      />
     </div>
   );
 }
