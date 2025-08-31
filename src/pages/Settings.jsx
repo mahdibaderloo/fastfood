f7fa9a57bc6633de4c97a807cf3f6864c884b090
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-31 12:55:08 +0330

    feedback for settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,8 @@
 import { RiSunLine } from "react-icons/ri";
 import HeaderSettings from "../features/settings/HeaderSettings";
+import { LiaBugSolid } from "react-icons/lia";
+import { CgChevronRight, CgDanger } from "react-icons/cg";
+import { LuSend } from "react-icons/lu";
 
 function Settings() {
   return (
@@ -9,10 +12,48 @@ function Settings() {
         <div>
           <p className="text-neutral-700">Theme</p>
           <div className="flex items-center justify-between p-2">
-            <p>Light mode</p>
+            <p className="text-neutral-900">Light mode</p>
             <p>
               <RiSunLine size={30} />
               {/* <BsMoonStarsFill /> */}
+            </p>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p className="text-neutral-700">Feedback</p>
+          <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
+            <div className="flex items-center gap-2">
+              <p>
+                <LiaBugSolid size={35} color="#171717" />
+              </p>
+              <p className="text-neutral-900">Report a bug</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
+            <div className="flex items-center gap-3">
+              <p>
+                <LuSend size={30} color="#171717" />
+              </p>
+              <p className="text-neutral-900">Send feedback</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-2">
+            <div className="flex items-center gap-3">
+              <p>
+                <CgDanger size={30} color="#171717" />
+              </p>
+              <p className="text-neutral-900">About us</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
             </p>
           </div>
         </div>
