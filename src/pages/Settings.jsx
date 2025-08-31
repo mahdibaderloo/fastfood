4eec38d4416bb3cd56427ea720c97032a351531d
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-08-31 12:38:05 +0330

    theme for settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,9 +1,22 @@
+import { RiSunLine } from "react-icons/ri";
 import HeaderSettings from "../features/settings/HeaderSettings";
 
 function Settings() {
   return (
     <>
       <HeaderSettings />
+      <main className="overflow-y-scroll p-4">
+        <div>
+          <p className="text-neutral-700">Theme</p>
+          <div className="flex items-center justify-between p-2">
+            <p>Light mode</p>
+            <p>
+              <RiSunLine size={30} />
+              {/* <BsMoonStarsFill /> */}
+            </p>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
