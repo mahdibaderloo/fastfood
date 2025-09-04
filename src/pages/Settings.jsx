010778c6049b1df518fe6581a5d2b48f064af52a
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-04 10:15:01 +0330

    general for settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -3,6 +3,10 @@ import HeaderSettings from "../features/settings/HeaderSettings";
 import { LiaBugSolid } from "react-icons/lia";
 import { CgChevronRight, CgDanger } from "react-icons/cg";
 import { LuSend } from "react-icons/lu";
+import { GoPerson, GoTrash } from "react-icons/go";
+import { IoLogOutOutline, IoNotificationsOutline } from "react-icons/io5";
+import { MdOutlineLogout } from "react-icons/md";
+import { HiOutlineTrash } from "react-icons/hi";
 
 function Settings() {
   return (
@@ -19,14 +23,15 @@ function Settings() {
             </p>
           </div>
         </div>
+
         <div className="mt-4">
-          <p className="text-neutral-700">Feedback</p>
+          <p className="text-neutral-700">General</p>
           <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
-            <div className="flex items-center gap-2">
+            <div className="flex items-center justify-start">
               <p>
-                <LiaBugSolid size={35} color="#171717" />
+                <GoPerson size={35} color="#171717" />
               </p>
-              <p className="text-neutral-900">Report a bug</p>
+              <p className="text-neutral-900 ml-3">Account</p>
             </div>
             <p>
               <CgChevronRight size={30} color="#2c2c2c96" />
@@ -34,11 +39,11 @@ function Settings() {
           </div>
 
           <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
-            <div className="flex items-center gap-3">
+            <div className="flex items-center justify-start">
               <p>
-                <LuSend size={30} color="#171717" />
+                <IoNotificationsOutline size={35} color="#171717" />
               </p>
-              <p className="text-neutral-900">Send feedback</p>
+              <p className="text-neutral-900 ml-3">Notifications</p>
             </div>
             <p>
               <CgChevronRight size={30} color="#2c2c2c96" />
@@ -46,17 +51,70 @@ function Settings() {
           </div>
 
           <div className="flex items-center justify-between p-2">
-            <div className="flex items-center gap-3">
+            <div className="flex items-center justify-start">
               <p>
-                <CgDanger size={30} color="#171717" />
+                <IoLogOutOutline size={36} color="#171717" />
               </p>
-              <p className="text-neutral-900">About us</p>
+              <p className="text-neutral-900 ml-3">Logout</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-2">
+            <div className="flex items-center justify-start">
+              <p>
+                <GoTrash size={34} color="#171717" />
+              </p>
+              <p className="text-neutral-900 ml-3">Delete account</p>
             </div>
             <p>
               <CgChevronRight size={30} color="#2c2c2c96" />
             </p>
           </div>
         </div>
+
+        <div className="mt-4">
+          <p className="text-neutral-700">Feedback</p>
+          <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
+            <div className="flex items-center">
+              <p>
+                <LiaBugSolid size={35} color="#171717" />
+              </p>
+              <p className="text-neutral-900 ml-3">Report a bug</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-2 border-b-2 border-[#2c2c2c1a]">
+            <div className="flex items-center">
+              <p>
+                <LuSend size={30} color="#171717" />
+              </p>
+              <p className="text-neutral-900 ml-4">Send feedback</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+
+          <div className="flex items-center justify-between p-2">
+            <div className="flex items-center">
+              <p>
+                <CgDanger size={30} color="#171717" />
+              </p>
+              <p className="text-neutral-900 ml-4">About us</p>
+            </div>
+            <p>
+              <CgChevronRight size={30} color="#2c2c2c96" />
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full h-24"></div>
       </main>
     </>
   );
