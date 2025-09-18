79d8699f4d18d2a12273d1aa09884fa493363190
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-18 19:07:25 +0330

    dashboard information styles

--- src/features/dashboard/UserInformation.jsx
+++ src/features/dashboard/UserInformation.jsx
@@ -2,15 +2,15 @@ import pizzaPattern from "../../data/images/PizzaPattern.jpg";
 
 function UserInformation() {
   return (
-    <div>
+    <div className="bg-amber-300 h-screen overflow-hidden">
       <div className="h-[150px] overflow-hidden object-contain">
         <img src={pizzaPattern} alt="pattern" className="" />
       </div>
       <div className="w-30 h-30 rounded-3xl bg-amber-50 absolute shadow top-10 left-0 right-0 m-auto">
         <img src="" alt="" />
       </div>
-      <div>
-        <h1>User Name</h1>
+      <div className="w-full flex justify-center pt-16">
+        <h1 className="text-amber-50 text-xl">Mahdi Baderloo</h1>
       </div>
     </div>
   );
