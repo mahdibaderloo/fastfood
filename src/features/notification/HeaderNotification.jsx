44122637b760db40969fef97304429f39b71553c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-19 23:08:56 +0330

    dark theme for HeaderNotification

--- src/features/notification/HeaderNotification.jsx
+++ src/features/notification/HeaderNotification.jsx
@@ -1,7 +1,7 @@
 function HeaderNotification() {
   return (
-    <header className="w-full flex items-center justify-center py-4 fixed bg-amber-300">
-      <p className="text-4xl">Not!f!cat!ons</p>
+    <header className="w-full flex items-center justify-center py-4 fixed bg-amber-300 dark:bg-neutral-800">
+      <p className="text-4xl dark:text-amber-300">Not!f!cat!ons</p>
     </header>
   );
 }
