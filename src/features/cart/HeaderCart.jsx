27fe2c3ef5d15e43d9a8797dfb5aabed3203cf20
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-19 22:56:14 +0330

    dark theme for HeaderCart

--- src/features/cart/HeaderCart.jsx
+++ src/features/cart/HeaderCart.jsx
@@ -1,7 +1,7 @@
 function HeaderCart() {
   return (
-    <header className="w-full flex items-center justify-center py-4 fixed bg-amber-300">
-      <p className="text-5xl">My Cart</p>
+    <header className="w-full flex items-center justify-center py-4 fixed bg-amber-300 dark:bg-neutral-800">
+      <p className="text-5xl dark:text-amber-300">My Cart</p>
     </header>
   );
 }
