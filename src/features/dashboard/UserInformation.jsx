e4af2e97ab890b6176cf2c41f207e20948849173
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-19 23:02:44 +0330

    dark theme for UserInformation

--- src/features/dashboard/UserInformation.jsx
+++ src/features/dashboard/UserInformation.jsx
@@ -3,15 +3,17 @@ import image1 from "../../data/images/1.jpg";
 
 function UserInformation() {
   return (
-    <div className="bg-amber-300">
+    <div className="bg-amber-300 dark:bg-neutral-800">
       <div className="h-[150px] overflow-hidden object-contain">
         <img src={pizzaPattern} alt="pattern" className="" />
       </div>
-      <div className="w-30 h-30 rounded-2xl bg-amber-300 absolute shadow top-10 left-0 right-0 m-auto overflow-hidden p-1">
+      <div className="w-30 h-30 rounded-2xl bg-amber-300 dark:bg-neutral-800 absolute shadow top-10 left-0 right-0 m-auto overflow-hidden p-1">
         <img src={image1} alt="" className="rounded-xl shadow object-cover" />
       </div>
       <div className="w-full flex justify-center pt-16">
-        <h1 className="text-amber-50 text-xl">Mahdi Baderloo</h1>
+        <h1 className="text-amber-50 dark:text-amber-300 text-xl">
+          Mahdi Baderloo
+        </h1>
       </div>
     </div>
   );
