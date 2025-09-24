75a946324f2baaec4088b334382e72c18695f0c5
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-09-24 20:23:56 +0330

    change theme for pizza icon

--- src/features/menu/HeaderMenu.jsx
+++ src/features/menu/HeaderMenu.jsx
@@ -2,15 +2,16 @@ import Filter from "../../components/Filter";
 import Search from "../../components/Search";
 import pizzaIcon from "../../data/images/pizza.svg";
 import pizzaDarkIcon from "../../data/images/pizza-dark.svg";
-
-const isDark = document.querySelector("html").className === "dark";
+import { useTheme } from "../../store/themeStore";
 
 function HeaderMenu() {
+  const { theme } = useTheme();
+
   return (
     <header className="w-full flex flex-col items-center justify-center py-4 fixed z-50 bg-amber-300 dark:bg-neutral-800">
       <div className="flex mb-4">
         <p className="text-6xl dark:text-amber-300">PIZZ</p>
-        {isDark ? (
+        {theme === "dark" ? (
           <img src={pizzaDarkIcon} alt="pizza" className="w-11 rotate-180" />
         ) : (
           <img src={pizzaIcon} alt="pizza" className="w-11 rotate-180" />
