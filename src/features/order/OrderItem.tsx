55c594a5872d207b82fc282dbd5148850c084973
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-10-22 00:16:26 +0330

    add OrderItem component

---
+++ src/features/order/OrderItem.tsx
@@ -0,0 +1,26 @@
+import { CgChevronRight } from "react-icons/cg";
+
+import pizza from "../../data/images/loading-1.png";
+
+function OrderItem() {
+  return (
+    <li className="bg-amber-100 rounded-lg p-3 relative flex flex-col justify-between">
+      <div className="flex justify-between text-neutral-800">
+        <p>Order: 4738929</p>
+        <p>$120</p>
+      </div>
+      <p className="text-sm text-neutral-600 pt-2">2025-10-10</p>
+      <div className="flex justify-between items-center pt-4">
+        <div className="flex divide-x-2 divide-neutral-600">
+          <img src={pizza} alt="pizza image" className="w-10 mx-1" />
+          <img src={pizza} alt="pizza image" className="w-10 mx-1" />
+        </div>
+        <p>
+          <CgChevronRight size={30} color="#525252" />
+        </p>
+      </div>
+    </li>
+  );
+}
+
+export default OrderItem;
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,7 +1,25 @@
+import Header from "../components/Header";
+import OrderItem from "../features/order/OrderItem";
+
 function Orders() {
   return (
     <>
-      <header>My Orders</header>
+      <Header
+        classHeader="w-full flex items-center justify-center py-4 fixed z-50 bg-amber-300 dark:bg-neutral-800"
+        pContent="My Orders"
+        classP="text-4xl dark:text-amber-300"
+      />
+      <main className="overflow-y-scroll p-4 pt-20 h-screen">
+        <ul className="flex flex-col gap-1">
+          <OrderItem />
+          <OrderItem />
+          <OrderItem />
+          <OrderItem />
+          <OrderItem />
+          <OrderItem />
+        </ul>
+        <div className="w-full h-16"></div>
+      </main>
     </>
   );
 }
