7cf7908a32d7b342b86d8ed3909784b97f7c9175
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-10-26 09:50:31 +0330

    add SupportForm component

---
+++ src/features/support/SupportForm.tsx
@@ -0,0 +1,45 @@
+import { BsPersonFill } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+import { FaPen } from "react-icons/fa6";
+
+function SupportForm() {
+  return (
+    <form>
+      <h3 className="text-amber-100">How can we help you?</h3>
+      <div className="flex items-center gap-2 bg-amber-100 p-2 rounded-lg mt-2">
+        <BsPersonFill size={40} color="#262626" />
+        <input
+          type="text"
+          className="border-none outline-none text-neutral-800 text-sm"
+          placeholder="Name"
+        />
+      </div>
+      <div className="flex items-center gap-2 bg-amber-100 p-2 rounded-lg mt-2">
+        <MdEmail size={40} color="#262626" />
+        <input
+          type="text"
+          className="border-none outline-none text-neutral-800 text-sm"
+          placeholder="Email"
+        />
+      </div>
+      <div>
+        <div className="flex items-start gap-2 bg-amber-100 p-2 rounded-lg mt-2 h-45">
+          <FaPen size={35} color="#262626" />
+          <textarea
+            className="border-none outline-none text-neutral-800 text-sm w-full h-full"
+            maxLength={200}
+            placeholder="Problem ..."
+          ></textarea>
+        </div>
+        <p className="text-amber-100 text-[16px] w-full flex justify-end mt-1 mr-4">
+          10 / 200
+        </p>
+      </div>
+      <button className="bg-amber-100 text-neutral-800 rounded-lg p-2 w-full hover:bg-amber-200 transition-all delay-100 mt-4">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default SupportForm;
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,5 +1,15 @@
+import Header from "../components/Header";
+import SupportForm from "../features/support/SupportForm";
+
 function Support() {
-  return <div>SUPPORT</div>;
+  return (
+    <>
+      <Header pContent="Support" />
+      <main className="p-4 pt-22 h-screen overflow-hidden">
+        <SupportForm />
+      </main>
+    </>
+  );
 }
 
 export default Support;
