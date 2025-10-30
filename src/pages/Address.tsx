650e3e9bc81a8006a12b3b869a3efc434625b2f8
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-10-30 10:17:48 +0330

    add AddNewAddress button

--- src/pages/Address.tsx
+++ src/pages/Address.tsx
@@ -4,12 +4,15 @@ import AddressItem from "../features/address/AddressItem";
 function Address() {
   return (
     <>
-      <Header pContent="Address" />
+      <Header pContent="ADDRESS" />
       <main className="p-4 pt-22 h-screen overflow-y-scroll">
-        <ul className="flex items-center justify-center flex-wrap gap-2">
+        <ul className="flex flex-col items-center justify-center flex-wrap gap-2">
           <AddressItem />
           <AddressItem />
         </ul>
+        <button className="bg-amber-200 text-neutral-800 w-full rounded-lg p-2 mt-10">
+          Add New Address
+        </button>
         <div className="w-full h-16"></div>
       </main>
     </>
