7446d61d7ea76d27cc0708db82b77dd284589d93
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-10-30 09:33:00 +0330

    add FavoritesItem component

---
+++ src/features/favorites/FavoritesItem.tsx
@@ -0,0 +1,20 @@
+import { GoHeartFill } from "react-icons/go";
+import pizza from "../../data/images/loading-1.png";
+
+function FavoritesItem() {
+  return (
+    <li className="bg-amber-50 rounded-lg w-[48%] flex flex-col p-2 relative">
+      <p className="self-end">
+        {/* <GoHeart size={30} color="#2c2c2c" /> */}
+        <GoHeartFill color="#2c2c2c" />
+      </p>
+      <img src={pizza} alt="product-image" className="w-full mt-2" />
+      <p className="text-lg text-neutral-900">Pizza</p>
+      {/* <p className="self-end bg-amber-200 absolute right-0 bottom-0 p-2 rounded-tl-lg rounded-br-lg">
+        <IoMdAdd />
+      </p> */}
+    </li>
+  );
+}
+
+export default FavoritesItem;
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,20 @@
+import Header from "../components/Header";
+import FavoritesItem from "../features/favorites/FavoritesItem";
+
 function Favorites() {
-  return <div>FAVORITES</div>;
+  return (
+    <>
+      <Header pContent="FAVOR!TES" />
+      <main className="overflow-y-scroll pt-22">
+        <ul className="flex items-center justify-center flex-wrap gap-2 mt-4 p-2">
+          <FavoritesItem />
+          <FavoritesItem />
+          <FavoritesItem />
+          <FavoritesItem />
+        </ul>
+      </main>
+    </>
+  );
 }
 
 export default Favorites;
