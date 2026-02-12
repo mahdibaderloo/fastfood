73aff9befa4c5a422c91120f37a889b5003794a7
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-12 16:26:01 +0330

    add PurchaseItem and edit styles

---
+++ src/features/cart/PurchaseItem.tsx
@@ -0,0 +1,28 @@
+import { FaMoneyBill1Wave, FaPizzaSlice } from "react-icons/fa6";
+import { GoNumber } from "react-icons/go";
+
+export default function PurchaseItem() {
+  return (
+    <li className="w-full h-20 bg-amber-100 p-2 rounded-2xl flex justify-between shadow">
+      <div className="flex flex-col justify-around">
+        <div className="flex items-center gap-2 text-neutral-800">
+          <FaPizzaSlice />
+          <span className="text-[0.65rem]">Title</span>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 text-neutral-800">
+            <FaMoneyBill1Wave />
+            <span className="text-[0.85rem]">10</span>
+          </div>
+          <div className="flex items-center gap-1 text-neutral-800">
+            <GoNumber />
+            <span className="text-[0.85rem]">2</span>
+          </div>
+        </div>
+      </div>
+      <div className="h-full w-16 bg-amber-50 rounded-2xl p-1">
+        <img src="" alt="item-image" className="" />
+      </div>
+    </li>
+  );
+}
--- src/pages/Purchase.tsx
+++ src/pages/Purchase.tsx
@@ -1,5 +1,7 @@
+import { CgDanger } from "react-icons/cg";
 import Header from "../components/Header";
 import PurchaseAddressBox from "../features/cart/PurchaseAddressBox";
+import PurchaseItem from "../features/cart/PurchaseItem";
 
 function Purchase() {
   return (
@@ -11,6 +13,19 @@ function Purchase() {
       />
       <main className="p-4 pt-19 h-screen overflow-y-scroll">
         <PurchaseAddressBox />
+        <div className="bg-white/40 flex items-center gap-1 p-2 rounded-xl mt-4">
+          <CgDanger size={45} color="#262626" />
+          <p className="text-neutral-800 text-[0.5rem] text-justify">
+            The invoice will be delivered together with your order. You can also
+            track and view your order anytime from your dashboard
+          </p>
+        </div>
+        <ul className="flex flex-col mt-4 gap-2">
+          <p className="text-[0.7rem] text-amber-100">Items: </p>
+          <PurchaseItem />
+          <PurchaseItem />
+        </ul>
+        <div className="w-full h-18"></div>
       </main>
     </>
   );
