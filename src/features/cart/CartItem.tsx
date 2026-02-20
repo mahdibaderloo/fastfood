b9958b7a5fe362bce5242b7df652793229edcdd0
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-20 13:34:58 +0330

    use cartStore for show items in cart

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -1,41 +1,42 @@
-import { useState } from "react";
-import pizza from "../../assets/images/loading-1.png";
+import { useCartStore } from "../../store/cartStore";
 
-function CartItem() {
-  const [count, setCount] = useState(1);
+interface Item {
+  id: number;
+  productName: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  comments: string[];
+  score: number;
+  count: number;
+}
 
-  function handleIncrease() {
-    if (count <= 19) {
-      setCount((count) => count + 1);
-    }
-  }
+type ItemProp = { item: Item };
 
-  function handleDecrease() {
-    if (count > 1) {
-      setCount((count) => count - 1);
-    }
-  }
+function CartItem({ item }: ItemProp) {
+  const { increaseItemCount, decreaseItemCount } = useCartStore();
 
   return (
     <li className="flex bg-amber-50 rounded-xl p-2 shadow w-[95%] mx-auto">
       <div className="bg-amber-200 w-18 rounded-xl shadow">
-        <img src={pizza} alt="product-image" className="w-full" />
+        <img src={item.image} alt="product-image" className="w-full" />
       </div>
       <div className=" flex flex-col justify-between ml-3">
-        <p className="text-neutral-900 text-xl">Title</p>
-        <p className="text-neutral-800">$20</p>
+        <p className="text-neutral-900 text-xl">{item.productName}</p>
+        <p className="text-neutral-800">${item.price}</p>
       </div>
       <div className="w-18 h-fit p-0.5 rounded-lg flex justify-between bg-amber-200 dark:bg-neutral-800 text-amber-800 mt-auto ml-auto">
         <span
           className="bg-amber-50 w-5 flex justify-center rounded-md"
-          onClick={handleDecrease}
+          onClick={() => decreaseItemCount(item.id)}
         >
           -
         </span>
-        <span className="text-amber-800">{count}</span>
+        <span className="text-amber-800">{item.count}</span>
         <span
           className="bg-amber-300 text-amber-800 w-5 flex justify-center rounded-md"
-          onClick={handleIncrease}
+          onClick={() => increaseItemCount(item.id)}
         >
           +
         </span>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,15 +1,18 @@
 import Header from "../components/Header.js";
 import CartItem from "../features/cart/CartItem.js";
 import CartPrice from "../features/cart/CartPrice.js";
+import { useCartStore } from "../store/cartStore.js";
 
 function Cart() {
+  const { items } = useCartStore();
+
   return (
     <>
       <Header pContent="Cart" />
       <main className="overflow-y-scroll pt-22">
         <ul className="flex flex-col gap-2">
-          <CartItem />
-          <CartItem />
+          {items.length > 0 &&
+            items.map((item) => <CartItem key={item.id} item={item} />)}
         </ul>
         <CartPrice />
       </main>
