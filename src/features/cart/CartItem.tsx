9bbdde2cecdcef92d83a212294cd5086dddc5917
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-20 14:02:31 +0330

    edit cart styles

--- src/features/cart/CartItem.tsx
+++ src/features/cart/CartItem.tsx
@@ -18,14 +18,15 @@ function CartItem({ item }: ItemProp) {
   const { increaseItemCount, decreaseItemCount } = useCartStore();
 
   return (
-    <li className="flex bg-amber-50 rounded-xl p-2 shadow w-[95%] mx-auto">
+    <li className="flex items-center bg-amber-50 rounded-xl p-2 shadow w-[95%] mx-auto">
       <div className="bg-amber-200 w-19 h-14 rounded-xl shadow overflow-hidden flex justify-center items-center p-0.5">
         <img src={item.image} alt="product-image" className="w-full" />
       </div>
-      <div className="w-full flex flex-col justify-between items-center p-1 pl-2">
+      <div className="w-full flex flex-col gap-4 justify-between items-center p-1 pl-2">
         <p className="text-neutral-900 text-[0.75rem] w-full">
           {item.productName}
         </p>
+
         <div className="w-full flex items-center justify-between">
           <p className="text-neutral-800">${item.price}</p>
           <div className="w-18 h-fit p-0.5 rounded-lg flex justify-between bg-amber-200 dark:bg-neutral-800 text-amber-800 mt-auto ml-auto">
--- src/features/cart/CartPrice.tsx
+++ src/features/cart/CartPrice.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useCartStore } from "../../store/cartStore";
 
 function CartPrice() {
   const navigate = useNavigate();
+  const [delivery, setDelivery] = useState(10);
+  const { totalPrice, items } = useCartStore();
+  const isEmpty = items.length === 0;
 
   function handlePurchase(e: React.MouseEvent) {
     e.preventDefault();
@@ -25,11 +29,11 @@ function CartPrice() {
       <div>
         <div className="flex justify-between items-center mt-4">
           <span className="text-neutral-700">Sub total</span>
-          <p className="text-neutral-800">$120</p>
+          <p className="text-neutral-800">${totalPrice}</p>
         </div>
         <div className="flex justify-between items-center mt-2">
           <span className="text-neutral-700">Delivery</span>
-          <p className="text-neutral-800">$10</p>
+          <p className="text-neutral-800">${isEmpty ? 0 : delivery}</p>
         </div>
         <div className="flex justify-between items-center mt-2">
           <span className="text-neutral-700">Discount</span>
@@ -37,17 +41,28 @@ function CartPrice() {
         </div>
         <div className="flex justify-between items-center mt-4">
           <span className="text-neutral-700">Total</span>
-          <p className="text-neutral-800">$130</p>
+          <p className="text-neutral-800">
+            ${isEmpty ? 0 : totalPrice + delivery}
+          </p>
         </div>
         <button
-          className="bg-amber-200 dark:bg-neutral-800 dark:text-amber-300 text-amber-800 p-2 rounded-lg w-full mt-6 inline-block relative overflow-hidden animate-bounce"
+          className={`p-2 rounded-lg w-full mt-6 inline-block relative overflow-hidden ${isEmpty ? "hidden invisible" : "animate-bounce bg-amber-200 dark:bg-neutral-800 dark:text-amber-300 text-amber-800"}`}
           onClick={handlePurchase}
+          disabled={isEmpty}
         >
           Purchase
-          <span className="absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 animate-spin-border"></span>
-          <span className="absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 animate-spin-border animation-delay-25"></span>
-          <span className="absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 animate-spin-border delay-150 animation-delay-50"></span>
-          <span className="absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 animate-spin-border delay-300 animation-delay-75"></span>
+          <span
+            className={`absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 ${isEmpty ? "hidden" : "animate-spin-border"}`}
+          ></span>
+          <span
+            className={`absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 ${isEmpty ? "hidden" : "animate-spin-border animation-delay-25"}`}
+          ></span>
+          <span
+            className={`absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 ${isEmpty ? "hidden" : "animate-spin-border delay-150 animation-delay-50"}`}
+          ></span>
+          <span
+            className={`absolute top-0 left-0 rounded-2xl w-[6px] h-[6px] bg-amber-800 dark:bg-amber-300 ${isEmpty ? "hidden" : "animate-spin-border delay-300 animation-delay-75"}`}
+          ></span>
         </button>
       </div>
     </form>
