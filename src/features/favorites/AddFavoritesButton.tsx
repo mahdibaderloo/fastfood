c5d1aa62ec8b400e0c64762d3eef89886cde3ab1
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-25 20:23:59 +0330

    add new favorite item link

--- src/features/favorites/AddFavoritesButton.tsx
+++ src/features/favorites/AddFavoritesButton.tsx
@@ -1,14 +1,17 @@
 import { IoMdAdd } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 function AddFavoritesButton() {
   return (
     <li className="bg-amber-50 rounded-lg w-full flex flex-col p-2 relative h-[50px]">
-      <p className="h-full flex justify-center items-center">
-        <IoMdAdd size={30} color="#525252" />
-        <span className="text-[0.7rem] text-neutral-600 text-shadow-sm">
-          Add new favorite item
-        </span>
-      </p>
+      <Link to="/">
+        <p className="h-full flex justify-center items-center">
+          <IoMdAdd size={30} color="#525252" />
+          <span className="text-[0.7rem] text-neutral-600 text-shadow-sm">
+            Add new favorite item
+          </span>
+        </p>
+      </Link>
     </li>
   );
 }
