a25ae45dcc18d27f5ed64baa216ca23330f99df2
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-25 21:26:33 +0330

    handle link to Item page

--- src/features/menu/MenuItem.tsx
+++ src/features/menu/MenuItem.tsx
@@ -3,6 +3,8 @@ import { IoMdAdd, IoMdRemove } from "react-icons/io";
 import { useCartStore } from "../../store/cartStore";
 import toast from "react-hot-toast";
 import { useFavoritesStore } from "../../store/favoritesStore";
+import { Link } from "react-router-dom";
+import { slugify } from "../../utils/slugify";
 
 interface Item {
   item: {
@@ -51,46 +53,48 @@ function MenuItem({ item }: Item) {
 
   return (
     <li className="bg-amber-50 rounded-lg w-[48%] flex flex-col p-2 relative">
-      <p className="self-end">
-        {isItemInFavorites ? (
-          <GoHeartFill
-            size={30}
-            color="#2c2c2c"
-            onClick={handleRemoveItemFromFavorites}
-          />
+      <Link to={`/${slugify(item.productName)}`}>
+        <p className="self-end">
+          {isItemInFavorites ? (
+            <GoHeartFill
+              size={30}
+              color="#2c2c2c"
+              onClick={handleRemoveItemFromFavorites}
+            />
+          ) : (
+            <GoHeart
+              size={30}
+              color="#2c2c2c"
+              onClick={handleAddItemToFavorites}
+            />
+          )}
+        </p>
+        <img
+          src={image}
+          alt="product-image"
+          className="w-full h-24 mt-2 object-cover rounded-lg"
+          loading="lazy"
+        />
+        <p className="text-[0.6rem] text-neutral-900 mt-2">{productName}</p>
+        <p className="mt-2 text-neutral-800">${price}</p>
+        {isItemInCart ? (
+          <button
+            title="Remove from cart"
+            className="self-end bg-amber-200 absolute right-0 bottom-0 p-2 rounded-tl-lg rounded-br-lg"
+            onClick={handleRemoveItemFromCart}
+          >
+            <IoMdRemove />
+          </button>
         ) : (
-          <GoHeart
-            size={30}
-            color="#2c2c2c"
-            onClick={handleAddItemToFavorites}
-          />
+          <button
+            title="Add to cart"
+            className="self-end bg-amber-200 absolute right-0 bottom-0 p-2 rounded-tl-lg rounded-br-lg"
+            onClick={handleAddItemToCart}
+          >
+            <IoMdAdd />
+          </button>
         )}
-      </p>
-      <img
-        src={image}
-        alt="product-image"
-        className="w-full h-24 mt-2 object-cover rounded-lg"
-        loading="lazy"
-      />
-      <p className="text-[0.6rem] text-neutral-900 mt-2">{productName}</p>
-      <p className="mt-2 text-neutral-800">${price}</p>
-      {isItemInCart ? (
-        <button
-          title="Remove from cart"
-          className="self-end bg-amber-200 absolute right-0 bottom-0 p-2 rounded-tl-lg rounded-br-lg"
-          onClick={handleRemoveItemFromCart}
-        >
-          <IoMdRemove />
-        </button>
-      ) : (
-        <button
-          title="Add to cart"
-          className="self-end bg-amber-200 absolute right-0 bottom-0 p-2 rounded-tl-lg rounded-br-lg"
-          onClick={handleAddItemToCart}
-        >
-          <IoMdAdd />
-        </button>
-      )}
+      </Link>
     </li>
   );
 }
