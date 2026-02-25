ce88d92747bbe46f0570cf818e159ff01e58e234
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-02-25 20:09:35 +0330

    fix show items by search bug

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -8,10 +8,44 @@ import pizzaIcon from "../assets/images/pizza.svg";
 import pizzaDarkIcon from "../assets/images/pizza-dark.svg";
 import { useFoods } from "../hooks/useFoods.js";
 import Loading from "../components/Loading.js";
+import { useSearchParams } from "react-router-dom";
+import { deSlugify } from "../utils/slugify.js";
+import { useEffect, useState } from "react";
+
+interface Food {
+  id: number;
+  productName: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  comments: string[];
+  score: number;
+}
 
 function Menu() {
+  const [filteredSearchItems, setFilteredSearchItems] = useState<Food[]>([]);
   const { theme } = useTheme();
   const { data: foods, isLoading } = useFoods();
+
+  const [searchParams] = useSearchParams();
+  const slug = searchParams.get("search") || "";
+  const filterSearchParam = deSlugify(slug);
+
+  useEffect(() => {
+    if (!foods) return;
+
+    if (!slug) {
+      setFilteredSearchItems(foods);
+      return;
+    }
+
+    const result = foods.filter((food) =>
+      food.productName.toLowerCase().includes(filterSearchParam.toLowerCase()),
+    );
+
+    setFilteredSearchItems(result);
+  }, [slug, foods, filterSearchParam]);
 
   if (isLoading) return <Loading />;
 
@@ -37,9 +71,10 @@ function Menu() {
 
       <main className="overflow-y-scroll pt-22">
         <ul className="flex flex-wrap items-center justify-center gap-2 mt-14 p-2">
-          {foods?.map((food) => (
-            <MenuItem key={food.id} item={food} />
-          ))}
+          {filteredSearchItems.length > 0 &&
+            filteredSearchItems.map((food) => (
+              <MenuItem key={food.id} item={food} />
+            ))}
         </ul>
         <div className="w-full h-16"></div>
       </main>
