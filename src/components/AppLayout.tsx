99f597f367f73158d36900873e565b53f027d93d
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-02 12:58:17 +0330

    Item page

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
-import Loading from "./Loading";
 
 function AppLayout() {
   return (
--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -7,6 +7,8 @@ import { useFavoritesStore } from "../store/favoritesStore";
 import { deSlugify } from "../utils/slugify";
 
 import Loading from "../components/Loading";
+import Header from "../components/Header";
+import { GoHeart, GoHeartFill } from "react-icons/go";
 
 function Item() {
   const { data: items, isLoading } = useFoods();
@@ -36,7 +38,43 @@ function Item() {
 
   if (isLoading) return <Loading />;
 
-  return <div>{item?.productName}</div>;
+  return (
+    <>
+      <Header showBackButton={true}>
+        <div className="mr-3">
+          {item?.isFavorite ? (
+            <GoHeartFill
+              size={30}
+              color="#ffd230"
+              // onClick={handleRemoveItemFromFavorites}
+            />
+          ) : (
+            <GoHeart
+              size={30}
+              color="#ffd230"
+              // onClick={handleAddItemToFavorites}
+            />
+          )}
+        </div>
+      </Header>
+      <main className="min-h-screen overflow-y-scroll pt-15 flex flex-col items-center">
+        <div className="bg-amber-300 dark:bg-neutral-400 w-[100%] h-50 p-2 rounded-full overflow-hidden">
+          <img src={item?.image} alt="item-image" />
+        </div>
+        <div className="bg-amber-300 h-full mt-2">
+          <h3>{item?.productName}</h3>
+          <p>{item?.price}</p>
+          <p>{item?.description}</p>
+          <button>Add To Cart</button>
+        </div>
+        <ul>
+          {item?.comments.map((comment, index) => (
+            <li key={index}>{comment}</li>
+          ))}
+        </ul>
+      </main>
+    </>
+  );
 }
 
 export default Item;
