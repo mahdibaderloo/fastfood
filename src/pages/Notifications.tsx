b969bf87aac98f41b74038d8a91fe72c6d21944c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-04 10:44:17 +0330

    responsive Notifications (tablet size)

--- src/pages/Notifications.tsx
+++ src/pages/Notifications.tsx
@@ -5,7 +5,7 @@ function Notifications() {
   return (
     <>
       <Header classP="text-4xl dark:text-amber-300" pContent="Not!f!cat!ons" />
-      <main className="overflow-y-scroll p-4 pt-20 h-screen">
+      <main className="overflow-y-scroll p-4 sm:px-26 pt-20 h-screen">
         <ul className="flex flex-col gap-1">
           <NotificationItem id={1} isUnRead={true} />
           <NotificationItem id={2} isUnRead={true} />
