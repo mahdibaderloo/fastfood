48a6881cd267c2ac2b5add01fbbdc8643238a2b4
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-06 14:55:50 +0330

    responsive Purchase (tablet size)

--- src/features/cart/PurchaseAddressBox.tsx
+++ src/features/cart/PurchaseAddressBox.tsx
@@ -2,24 +2,26 @@ import { FaLocationDot } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 function PurchaseAddressBox() {
+  const width = window.innerWidth;
+
   return (
     <>
-      <div className="w-full bg-amber-100 p-3 rounded-2xl flex flex-col gap-2 relative">
+      <div className="w-full bg-amber-100 p-3 rounded-2xl flex flex-col gap-2 sm:gap-4 relative">
         <div className="flex justify-between items-center">
           <p>
-            <FaLocationDot size={30} color="#2c2c2c" />
+            <FaLocationDot size={width > 640 ? 34 : 30} color="#2c2c2c" />
           </p>
         </div>
-        <p className="text-neutral-900 text-[0.7rem] ml-2 w-[95%] text-justify">
+        <p className="text-neutral-900 text-[0.7rem] sm:text-[0.85rem] ml-2 w-[95%] text-justify">
           Mohammad shahr, Karaj, Iran
         </p>
-        <p className="text-[0.5rem] ml-2 text-neutral-600">
+        <p className="text-[0.5rem] sm:text-[0.65rem] ml-2 text-neutral-600">
           Postal code: 123456789
         </p>
       </div>
       <Link
         to="/address"
-        className="text-[0.5rem] text-blue-400 underline ml-2"
+        className="text-[0.5rem] sm:text-[0.65rem] text-blue-400 underline ml-2"
       >
         Edit address
       </Link>
--- src/pages/Purchase.tsx
+++ src/pages/Purchase.tsx
@@ -6,6 +6,7 @@ import { useCartStore } from "../store/cartStore";
 
 function Purchase() {
   const { items } = useCartStore();
+  const width = window.innerWidth;
 
   return (
     <>
@@ -14,28 +15,28 @@ function Purchase() {
         pContent="PURCHA$E"
         classP="text-4xl dark:text-amber-300"
       />
-      <main className="p-4 pt-19 h-screen overflow-y-scroll">
+      <main className="p-4 pt-19 h-screen overflow-y-scroll sm:w-90 sm:mx-auto">
         <PurchaseAddressBox />
-        <div className="bg-white/40 flex items-center gap-1 p-2 rounded-xl mt-4">
-          <CgDanger size={45} color="#262626" />
-          <p className="text-neutral-800 text-[0.5rem] text-justify">
+        <div className="bg-white/40 flex items-center gap-1 p-2 sm:p-3 rounded-xl mt-4 sm:mt-6">
+          <CgDanger size={width > 640 ? 55 : 45} color="#262626" />
+          <p className="text-neutral-800 text-[0.5rem] sm:text-[0.65rem] text-justify">
             The invoice will be delivered together with your order. You can also
             track and view your order anytime from your dashboard
           </p>
         </div>
-        <ul className="flex flex-col mt-4 gap-2">
-          <p className="text-[0.7rem] text-neutral-800 dark:text-amber-100">
+        <ul className="flex flex-col mt-4 sm:mt-6 gap-2">
+          <p className="text-[0.7rem] sm:text-[0.8rem] text-neutral-800 dark:text-amber-100">
             Items:{" "}
           </p>
           {items.map((item) => (
             <PurchaseItem key={item.id} item={item} />
           ))}
         </ul>
-        <div className="text-neutral-800 dark:text-amber-100 flex justify-between mt-4 px-6">
+        <div className="text-neutral-800 dark:text-amber-100 flex justify-between mt-4 sm:mt-6 sm:font-semibold px-6">
           <p>Total price: </p>
           <p>$175</p>
         </div>
-        <button className="bg-neutral-800 dark:bg-amber-300 text-amber-300 dark:text-neutral-800 p-2 rounded-lg w-full mt-6 shadow">
+        <button className="bg-neutral-800 dark:bg-amber-300 text-amber-300 dark:text-neutral-800 p-2 sm:p-3 rounded-lg sm:rounded-xl w-full mt-6 shadow">
           Proceed to payment
         </button>
         <div className="w-full h-2"></div>
