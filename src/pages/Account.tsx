14cb90e91cc822854989f14a81686aa2e9c7bf37
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-06 14:01:40 +0330

    responsive Account (tablet size)

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -23,50 +23,56 @@ function Account() {
         showBackButton={true}
         classP="text-4xl dark:text-amber-300"
       />
-      <main className="p-4 pt-22 h-screen overflow-y-scroll">
-        <div className="w-full relative">
-          <div className="w-20 h-20 rounded-full bg-amber-50 dark:bg-amber-300 shadow mx-auto overflow-hidden p-0.5">
+      <main className="p-4 pt-22 sm:pt-24 h-screen overflow-y-scroll">
+        <div className="w-full sm:w-80 sm:mx-auto relative">
+          <div className="w-20 h-20 sm:w-26 sm:h-26 rounded-full bg-amber-50 dark:bg-amber-300 shadow mx-auto overflow-hidden p-0.5">
             <img
               src={image1}
               alt=""
               className="rounded-full shadow object-cover"
             />
           </div>
-          <div className="bg-amber-50 dark:bg-amber-300 backdrop-blur-md w-5 h-5 rounded-full flex items-center justify-center absolute left-[58%] bottom-1">
+          <div className="bg-amber-50 dark:bg-amber-300 backdrop-blur-md w-5 h-5 sm:w-6 sm:h-6 rounded-full flex items-center justify-center absolute left-[58%] bottom-1">
             <FaPen size={18} color="#2c2c2c" />
           </div>
         </div>
 
-        <form className="mt-8 p-2 bg-amber-50 dark:bg-amber-50/20 backdrop-blur-md rounded-2xl">
+        <form className="mt-8 p-2 bg-amber-50 dark:bg-amber-50/20 backdrop-blur-md rounded-2xl sm:w-80 sm:h-50 sm:mx-auto">
           <div className="p-2">
-            <p className="mb-1 text-neutral-800 dark:text-amber-300">Name</p>
+            <p className="mb-1 sm:mb-1.5 text-neutral-800 dark:text-amber-300">
+              Name
+            </p>
             <input
               type="text"
               placeholder="Mahdi Baderloo"
-              className="bg-amber-200 dark:bg-neutral-800 text-neutral-800 dark:text-amber-300 p-2 rounded-lg w-full outline-none border-none"
+              className="bg-amber-200 dark:bg-neutral-800 text-neutral-800 dark:text-amber-300 p-2 sm:p-2.5 rounded-lg w-full outline-none border-none"
             />
           </div>
-          <div className="p-2">
-            <p className="mb-1 text-neutral-800 dark:text-amber-300">Phone</p>
+          <div className="p-2 sm:mt-1">
+            <p className="mb-1 sm:mb-1.5 text-neutral-800 dark:text-amber-300">
+              Phone
+            </p>
             <input
               type="number"
               placeholder="0910*****98"
-              className="bg-amber-200 dark:bg-neutral-800 text-neutral-800 dark:text-amber-300 p-2 rounded-lg w-full outline-none border-none"
+              className="bg-amber-200 dark:bg-neutral-800 text-neutral-800 dark:text-amber-300 p-2 sm:p-2.5 rounded-lg w-full outline-none border-none"
             />
           </div>
         </form>
-        <button
-          className="w-full h-10 mt-2 bg-amber-50 dark:bg-amber-50/20 text-amber-300 rounded-xl"
-          onClick={handleOpenSaveModal}
-        >
-          Save
-        </button>
-        <button
-          className="w-full h-10 mt-2 bg-amber-50 dark:bg-amber-50/20 text-amber-950 rounded-xl"
-          onClick={handleOpenLogoutModal}
-        >
-          Logout
-        </button>
+        <div className="sm:w-80 sm:mx-auto sm:mt-6">
+          <button
+            className="w-full h-10 sm:h-11 mt-2 bg-amber-50 dark:bg-amber-50/20 text-amber-300 rounded-xl sm:text-lg sm:mx-auto"
+            onClick={handleOpenSaveModal}
+          >
+            Save
+          </button>
+          <button
+            className="w-full h-10 sm:h-11 mt-2 bg-amber-50 dark:bg-amber-50/20 text-amber-950 rounded-xl sm:text-lg sm:mx-auto"
+            onClick={handleOpenLogoutModal}
+          >
+            Logout
+          </button>
+        </div>
         <Modal />
       </main>
     </>
