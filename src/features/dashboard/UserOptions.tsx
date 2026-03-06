41577bac61974ca5e1b93fbee9e6deb58b960bfc
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-06 13:37:46 +0330

    refactore Dashboard options

--- src/features/dashboard/UserOptions.tsx
+++ src/features/dashboard/UserOptions.tsx
@@ -1,100 +1,93 @@
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "../../store/themeStore.js";
+
 import { FaLocationDot } from "react-icons/fa6";
 import { IoWallet } from "react-icons/io5";
 import { MdManageAccounts } from "react-icons/md";
 import { PiHandbagFill } from "react-icons/pi";
 import { TbJewishStarFilled } from "react-icons/tb";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
-import { useTheme } from "../../store/themeStore.js";
-import { Link } from "react-router-dom";
+
+interface Option {
+  id: number;
+  title: string;
+  path: string;
+  lightIcon: ReactNode;
+  darkIcon: ReactNode;
+  tabletSize: number;
+}
+
+const options: Option[] = [
+  {
+    id: 1,
+    title: "Account",
+    path: "/account",
+    lightIcon: <MdManageAccounts size={56} color="#262626" />,
+    darkIcon: <MdManageAccounts size={56} color="#ffd230" />,
+    tabletSize: 60,
+  },
+  {
+    id: 2,
+    title: "Orders",
+    path: "/orders",
+    lightIcon: <PiHandbagFill size={50} color="#262626" />,
+    darkIcon: <PiHandbagFill size={50} color="#ffd230" />,
+    tabletSize: 60,
+  },
+  {
+    id: 3,
+    title: "Favorites",
+    path: "/favorites",
+    lightIcon: <TbJewishStarFilled size={50} color="#262626" />,
+    darkIcon: <TbJewishStarFilled size={50} color="#ffd230" />,
+    tabletSize: 60,
+  },
+  {
+    id: 4,
+    title: "Wallet",
+    path: "/wallet",
+    lightIcon: <IoWallet size={50} color="#262626" />,
+    darkIcon: <IoWallet size={50} color="#ffd230" />,
+    tabletSize: 60,
+  },
+  {
+    id: 5,
+    title: "Address",
+    path: "/address",
+    lightIcon: <FaLocationDot size={50} color="#262626" />,
+    darkIcon: <FaLocationDot size={50} color="#ffd230" />,
+    tabletSize: 60,
+  },
+  {
+    id: 6,
+    title: "Support",
+    path: "/support",
+    lightIcon: <TfiHeadphoneAlt size={50} color="#262626" />,
+    darkIcon: <TfiHeadphoneAlt size={50} color="#ffd230" />,
+    tabletSize: 60,
+  },
+];
 
 function UserOptions() {
   const { theme } = useTheme();
 
   return (
     <div className="bg-amber-50 dark:bg-amber-50/20 backdrop-blur-md m-4 mt-13 p-2 rounded-xl sm:w-90 sm:h-67 sm:mx-auto flex flex-wrap justify-between gap-1 sm:gap-y-2">
-      <Link
-        to="/account"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <MdManageAccounts size={56} color="#ffd230" />
-          ) : (
-            <MdManageAccounts size={56} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Account</p>
-      </Link>
-
-      <Link
-        to="/orders"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <PiHandbagFill size={50} color="#ffd230" />
-          ) : (
-            <PiHandbagFill size={50} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Orders</p>
-      </Link>
-
-      <Link
-        to="/favorites"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <TbJewishStarFilled size={50} color="#ffd230" />
-          ) : (
-            <TbJewishStarFilled size={50} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Favorites</p>
-      </Link>
-
-      <Link
-        to="/wallet"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <IoWallet size={50} color="#ffd230" />
-          ) : (
-            <IoWallet size={50} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Wallet</p>
-      </Link>
-
-      <Link
-        to="/address"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <FaLocationDot size={50} color="#ffd230" />
-          ) : (
-            <FaLocationDot size={50} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Address</p>
-      </Link>
-
-      <Link
-        to="/support"
-        className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
-      >
-        <p className="w-20 h-16 flex justify-center items-center">
-          {theme === "dark" ? (
-            <TfiHeadphoneAlt size={50} color="#ffd230" />
-          ) : (
-            <TfiHeadphoneAlt size={50} color="#262626" />
-          )}
-        </p>
-        <p className="text-sm dark:text-amber-300">Support</p>
-      </Link>
+      {options.map((option) => (
+        <Link
+          key={option.id}
+          to={option.path}
+          className="bg-amber-100 dark:bg-neutral-800 w-[31%] sm:w-[32%] p-2 rounded-lg flex flex-col justify-center items-center gap-2 shadow"
+        >
+          <p className="w-20 h-16 flex justify-center items-center">
+            {theme === "dark" ? option.darkIcon : option.lightIcon}
+          </p>
+          <p className="text-sm sm:text-[1rem] dark:text-amber-300">
+            {option.title}
+          </p>
+        </Link>
+      ))}
     </div>
   );
 }
