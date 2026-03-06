760e73b1a4b9b32a3157a97c90c2b2a2eaaa7a56
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-06 14:28:07 +0330

    responsive Wallet (tablet size)

--- src/features/wallet/Balance.tsx
+++ src/features/wallet/Balance.tsx
@@ -4,35 +4,38 @@ import { useTheme } from "../../store/themeStore";
 
 function Balance() {
   const { theme } = useTheme();
+  const width = window.innerWidth;
 
   return (
-    <div className="bg-neutral-800 dark:bg-amber-300 w-[85%] mx-auto rounded-xl">
-      <div className="bg-amber-100 w-full mx-auto rounded-xl p-2 flex flex-col items-center shadow">
-        <p className="text-neutral-500 text-sm">Total Balance</p>
-        <p className="text-neutral-800 mt-1 text-4xl">$94.00</p>
+    <div className="bg-neutral-800 dark:bg-amber-300 w-[85%] mx-auto rounded-xl sm:w-70 sm:mx-auto">
+      <div className="bg-amber-100 w-full mx-auto rounded-xl p-2 sm:p-3 flex flex-col items-center shadow">
+        <p className="text-neutral-500 text-sm sm:text-[0.9rem]">
+          Total Balance
+        </p>
+        <p className="text-neutral-800 mt-1 sm:mt-2 text-4xl">$94.00</p>
       </div>
-      <ul className="flex items-center justify-evenly pt-2 pb-1">
+      <ul className="flex items-center justify-evenly pt-2 pb-1 sm:p-3">
         <li className="flex flex-col items-center justify-center">
-          <span className="bg-amber-300 dark:bg-neutral-800 p-2 rounded-full">
+          <span className="bg-amber-300 dark:bg-neutral-800 p-2 sm:p-3 rounded-full">
             {theme === "dark" ? (
-              <GiPayMoney size={30} color="#fef3c6" />
+              <GiPayMoney size={width > 640 ? 36 : 30} color="#fef3c6" />
             ) : (
-              <GiPayMoney size={30} color="#262626" />
+              <GiPayMoney size={width > 640 ? 36 : 30} color="#262626" />
             )}
           </span>
-          <p className="text-[16px] text-amber-300 dark:text-neutral-800">
+          <p className="text-[0.6rem] sm:text-[0.7rem] sm:mt-1 text-amber-300 dark:text-neutral-800">
             Deposit
           </p>
         </li>
         <li className="flex flex-col items-center justify-center">
-          <span className="bg-amber-300 dark:bg-neutral-800 p-2 rounded-full">
+          <span className="bg-amber-300 dark:bg-neutral-800 p-2 sm:p-3 rounded-full">
             {theme === "dark" ? (
-              <GiReceiveMoney size={30} color="#fef3c6" />
+              <GiReceiveMoney size={width > 640 ? 36 : 30} color="#fef3c6" />
             ) : (
-              <GiReceiveMoney size={30} color="#262626" />
+              <GiReceiveMoney size={width > 640 ? 36 : 30} color="#262626" />
             )}
           </span>
-          <p className="text-[16px] text-amber-300 dark:text-neutral-800">
+          <p className="text-[0.6rem] sm:text-[0.7rem] sm:mt-1 text-amber-300 dark:text-neutral-800">
             Withdraw
           </p>
         </li>
--- src/features/wallet/Transactions.tsx
+++ src/features/wallet/Transactions.tsx
@@ -1,38 +1,40 @@
 function Transactions() {
   return (
-    <div className="bg-amber-100 h-[400px] mt-6 p-2 pb-5 rounded-xl overflow-hidden">
-      <p className="text-neutral-800 text-sm h-7">Transactions History</p>
+    <div className="bg-amber-100 h-70 sm:h-74 sm:w-90 sm:mx-auto mt-6 p-2 pb-5 rounded-xl overflow-hidden">
+      <p className="text-neutral-800 text-sm sm:text-[1rem] h-7 sm:h-10">
+        Transactions History
+      </p>
       <div className="overflow-y-scroll h-[95%] scrollbar-hide">
-        <ul className="flex flex-col gap-1 overflow-y-scroll pb-2">
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+        <ul className="flex flex-col gap-1 overflow-y-scroll pb-2 sm:pb-3.5">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Buy Pizza</p>
             <span className="text-sm text-red-500">- $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Deposit</p>
             <span className="text-sm text-green-500">+ $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Buy Pizza</p>
             <span className="text-sm text-red-500">- $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Deposit</p>
             <span className="text-sm text-green-500">+ $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Buy Pizza</p>
             <span className="text-sm text-red-500">- $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Deposit</p>
             <span className="text-sm text-green-500">+ $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Buy Pizza</p>
             <span className="text-sm text-red-500">- $128.00</span>
           </li>
-          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg">
+          <li className="flex items-center justify-between bg-amber-50 p-2 shadow rounded-lg sm:h-10 sm:p-4">
             <p className="text-sm text-neutral-800">Deposit</p>
             <span className="text-sm text-green-500">+ $128.00</span>
           </li>
