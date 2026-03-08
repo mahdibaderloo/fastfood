e75cd1b2852fb079de2de07bef14fc3dd156126c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-08 15:50:22 +0330

    responsive Orders (laptop size)

--- src/features/order/OrderItem.tsx
+++ src/features/order/OrderItem.tsx
@@ -5,16 +5,18 @@ import pizza from "../../assets/images/loading-1.png";
 
 function OrderItem() {
   return (
-    <li className="rounded-lg">
+    <li className="rounded-lg lg:w-50">
       <Link
         to={`/orders/4738929`}
-        className="bg-amber-100 rounded-lg p-3 relative flex flex-col justify-between hover:bg-amber-200/70"
+        className="bg-amber-100 rounded-lg p-3 relative flex flex-col justify-between lg:hover:bg-amber-200 transition-all"
       >
         <div className="flex justify-between text-neutral-800">
-          <p>Order: 4738929</p>
-          <p>$120</p>
+          <p className="lg:text-sm">Order: 4738929</p>
+          <p className="lg:text-sm">$120</p>
         </div>
-        <p className="text-sm text-neutral-600 pt-2">2025-10-10</p>
+        <p className="text-sm lg:text-[0.7rem] text-neutral-600 pt-2">
+          2025-10-10
+        </p>
         <div className="flex justify-between items-center pt-4">
           <div className="flex divide-x-2 divide-neutral-600">
             <img src={pizza} alt="pizza image" className="w-10 mx-1" />
--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,3 +1,4 @@
+import { CgChevronLeft, CgChevronRight } from "react-icons/cg";
 import BackButton from "../components/BackButton";
 import Header from "../components/Header";
 import OrderItem from "../features/order/OrderItem";
@@ -11,9 +12,9 @@ function Orders() {
         showBackButton={true}
         classP="text-4xl dark:text-amber-300"
       />
-      <main className="overflow-y-scroll p-4 pt-20 h-screen">
+      <main className="overflow-y-scroll lg:overflow-hidden lg:w-[80%] lg:mx-auto p-4 pt-20 lg:pt-16 h-screen">
         <BackButton />
-        <ul className="flex flex-col gap-1 sm:gap-2 sm:w-90 sm:mx-auto">
+        <ul className="flex flex-col lg:flex-row lg:justify-center lg:flex-wrap gap-1 sm:gap-2 sm:w-90 lg:w-full lg:mt-4 sm:mx-auto lg:overflow-y-auto">
           <OrderItem />
           <OrderItem />
           <OrderItem />
@@ -21,7 +22,20 @@ function Orders() {
           <OrderItem />
           <OrderItem />
         </ul>
-        <div className="w-full h-2"></div>
+        <div className="w-full h-2 lg:hidden"></div>
+        <div className="hidden lg:flex w-full mt-6 justify-center items-center gap-2 text-neutral-800 dark:text-amber-200">
+          <CgChevronLeft
+            size={22}
+            className="text-neutral-700 dark:text-amber-200"
+          />
+          <span className="cursor-pointer">1</span>
+          <span className="cursor-pointer">2</span>
+          <span className="cursor-pointer">3</span>
+          <CgChevronRight
+            size={22}
+            className="text-neutral-700 dark:text-amber-200"
+          />
+        </div>
       </main>
     </>
   );
