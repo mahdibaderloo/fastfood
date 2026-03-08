4cf90ff77fb7af2ff636aacb653de839fe98e620
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-08 16:38:49 +0330

    add GeneralSettings

---
+++ src/features/settings/GeneralSettings.tsx
@@ -0,0 +1,70 @@
+import { RiSunLine } from "react-icons/ri";
+import { GoPerson, GoTrash } from "react-icons/go";
+import { IoLogOutOutline } from "react-icons/io5";
+import { BsMoonStarsFill } from "react-icons/bs";
+import SettingsItem from "./SettingsItem";
+import { useModalStore } from "../../store/modalStore";
+import { useTheme } from "../../store/themeStore";
+
+const generalItems = [
+  {
+    id: 1,
+    title: "Account",
+    darkIcon: <GoPerson size={35} color="#ffd230" />,
+    lightIcon: <GoPerson size={35} color="#171717" />,
+    url: "/account",
+  },
+  {
+    id: 2,
+    title: "Logout",
+    darkIcon: <IoLogOutOutline size={36} color="#ffd230" />,
+    lightIcon: <IoLogOutOutline size={36} color="#171717" />,
+    url: undefined,
+  },
+  {
+    id: 3,
+    title: "Delete Account",
+    darkIcon: <GoTrash size={34} color="#ffd230" />,
+    lightIcon: <GoTrash size={34} color="#171717" />,
+    url: undefined,
+  },
+];
+
+function GeneralSettings() {
+  const { theme } = useTheme();
+  const { open } = useModalStore();
+
+  function handleLogoutModal() {
+    open({ title: "Are you sure you want to logout?" });
+  }
+
+  function handleDeleteAccountModal() {
+    open({
+      title:
+        "Are you sure you want to DELETE YOUR ACCOUNT? (this action cannot be undone!)",
+    });
+  }
+  return (
+    <div className="mt-4 sm:w-90 sm:mx-auto">
+      <p className="text-amber-800 dark:text-amber-100 text-shadow-lg sm:text-lg">
+        General
+      </p>
+      <ul>
+        {generalItems.map((item) => (
+          <SettingsItem
+            key={item.id}
+            title={item.title}
+            url={item.url}
+            onClick={
+              item.id === 2 ? handleLogoutModal : handleDeleteAccountModal
+            }
+          >
+            <p>{theme === "dark" ? item.darkIcon : item.lightIcon}</p>
+          </SettingsItem>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default GeneralSettings;
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,42 +1,14 @@
 import { useTheme } from "../store/themeStore.js";
 
-import { RiSunLine } from "react-icons/ri";
 import { LiaBugSolid } from "react-icons/lia";
 import { CgDanger } from "react-icons/cg";
 import { LuSend } from "react-icons/lu";
-import { GoPerson, GoTrash } from "react-icons/go";
-import { IoLogOutOutline } from "react-icons/io5";
-import { BsMoonStarsFill } from "react-icons/bs";
 
 import Header from "../components/Header.js";
-import SettingsItem from "../features/settings/SettingsItem.js";
 import Modal from "../components/Modal.js";
-import { useModalStore } from "../store/modalStore.js";
+import SettingsItem from "../features/settings/SettingsItem.js";
 import ToggleThemeBox from "../features/settings/ToggleThemeBox.js";
-
-const generalItems = [
-  {
-    id: 1,
-    title: "Account",
-    darkIcon: <GoPerson size={35} color="#ffd230" />,
-    lightIcon: <GoPerson size={35} color="#171717" />,
-    url: "/account",
-  },
-  {
-    id: 2,
-    title: "Logout",
-    darkIcon: <IoLogOutOutline size={36} color="#ffd230" />,
-    lightIcon: <IoLogOutOutline size={36} color="#171717" />,
-    url: undefined,
-  },
-  {
-    id: 3,
-    title: "Delete Account",
-    darkIcon: <GoTrash size={34} color="#ffd230" />,
-    lightIcon: <GoTrash size={34} color="#171717" />,
-    url: undefined,
-  },
-];
+import GeneralSettings from "../features/settings/GeneralSettings.js";
 
 const feedbackItems = [
   {
@@ -79,18 +51,6 @@ const feedbackItems = [
 
 function Settings() {
   const { theme } = useTheme();
-  const { open } = useModalStore();
-
-  function handleLogoutModal() {
-    open({ title: "Are you sure you want to logout?" });
-  }
-
-  function handleDeleteAccountModal() {
-    open({
-      title:
-        "Are you sure you want to DELETE YOUR ACCOUNT? (this action cannot be undone!)",
-    });
-  }
 
   return (
     <div className="min-h-screen bg-amber-300 dark:bg-neutral-800">
@@ -98,27 +58,9 @@ function Settings() {
         classHeader="w-full flex items-center justify-center pt-4"
         pContent="Sett!ngs"
       />
-      <main className="overflow-y-scroll p-4">
+      <main className="overflow-y-scroll p-4 lg:pt-16">
         <ToggleThemeBox />
-        <div className="mt-4 sm:w-90 sm:mx-auto">
-          <p className="text-amber-800 dark:text-amber-100 text-shadow-lg sm:text-lg">
-            General
-          </p>
-          <ul>
-            {generalItems.map((item) => (
-              <SettingsItem
-                key={item.id}
-                title={item.title}
-                url={item.url}
-                onClick={
-                  item.id === 2 ? handleLogoutModal : handleDeleteAccountModal
-                }
-              >
-                <p>{theme === "dark" ? item.darkIcon : item.lightIcon}</p>
-              </SettingsItem>
-            ))}
-          </ul>
-        </div>
+        <GeneralSettings />
         <div className="mt-4 sm:w-90 sm:mx-auto">
           <p className="text-amber-800 dark:text-amber-100 text-shadow-lg sm:text-lg">
             Feedback
