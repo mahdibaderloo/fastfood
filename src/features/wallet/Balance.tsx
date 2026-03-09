3852c78509bb1c78158047bd84940214608b5d98
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-09 15:22:39 +0330

    responsive Wallet (laptop size)

--- src/features/wallet/Balance.tsx
+++ src/features/wallet/Balance.tsx
@@ -8,11 +8,11 @@ function Balance() {
 
   return (
     <div className="bg-neutral-800 dark:bg-amber-300 lg:bg-amber-100 lg:dark:bg-amber-100 w-[85%] lg:w[35%] mx-auto rounded-xl lg:rounded-none sm:w-70 sm:mx-auto lg:mt-0 lg:border-r lg:border-amber-300/20">
-      <div className="bg-amber-100 lg:bg-amber-300 w-full mx-auto rounded-xl lg:rounded-t-none lg:rounded-l-none p-2 sm:p-3 flex flex-col items-center shadow">
-        <p className="text-neutral-500 text-sm sm:text-[0.9rem]">
+      <div className="bg-amber-100 lg:dark:bg-amber-300 lg:bg-neutral-800 w-full mx-auto rounded-xl lg:rounded-t-none lg:rounded-l-none p-2 sm:p-3 flex flex-col items-center shadow">
+        <p className="text-neutral-500 lg:text-amber-100 lg:dark:text-neutral-700 text-sm sm:text-[0.9rem]">
           Total Balance
         </p>
-        <p className="text-neutral-800 mt-1 sm:mt-2  lg:mt-4 text-4xl lg:text-5xl">
+        <p className="text-neutral-800 lg:text-amber-300 lg:dark:text-neutral-800 mt-1 sm:mt-2  lg:mt-4 text-4xl lg:text-5xl">
           $94.00
         </p>
       </div>
