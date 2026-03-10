802640e4e3f2e51fa693b40ff5b0341208d42b13
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-10 12:23:20 +0330

    responsive Purchase (laptop size)

--- src/features/cart/PurchaseItem.tsx
+++ src/features/cart/PurchaseItem.tsx
@@ -17,24 +17,24 @@ type ItemProp = { item: Item };
 
 export default function PurchaseItem({ item }: ItemProp) {
   return (
-    <li className="w-full h-20 sm:h-24 bg-amber-100 p-2 sm:p-3 rounded-2xl flex justify-between shadow">
+    <li className="w-full lg:w-[45%] h-20 sm:h-24 bg-amber-100 p-2 sm:p-3 rounded-2xl flex justify-between shadow">
       <div className="flex flex-col justify-around">
         <div className="flex items-center gap-2 text-neutral-800">
           <FaPizzaSlice />
-          <span className="text-[0.65rem] sm:text-[0.75rem]">
+          <span className="text-[0.65rem] sm:text-[0.75rem] lg:text-[0.8rem]">
             {item.productName}
           </span>
         </div>
         <div className="flex items-center gap-4 sm:gap-6 sm:mt-2">
           <div className="flex items-center gap-2 text-neutral-800">
             <FaMoneyBill1Wave />
-            <span className="text-[0.85rem] sm:text-[0.9rem]">
+            <span className="text-[0.85rem] sm:text-[0.9rem] lg:text-[1rem] lg:font-semibold">
               {item.price}
             </span>
           </div>
           <div className="flex items-center gap-1 text-neutral-800">
             <GoNumber />
-            <span className="text-[0.85rem] sm:text-[0.9rem]">
+            <span className="text-[0.85rem] sm:text-[0.9rem] lg:text-[1rem] lg:font-semibold">
               {item.count}
             </span>
           </div>
