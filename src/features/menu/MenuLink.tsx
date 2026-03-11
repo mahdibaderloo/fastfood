c0ea328abf4cb92eb62030a0794da50c58875f9f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-11 14:50:36 +0330

    responsive Menu and Header (desktop size)

--- src/features/menu/MenuLink.tsx
+++ src/features/menu/MenuLink.tsx
@@ -2,7 +2,7 @@ function MenuLink() {
   return (
     <a
       href="http://github.com/mahdibaderloo"
-      className="hidden lg:block text-[0.6rem] ml-1 text-amber-50 cursor-pointer"
+      className="hidden lg:block text-[0.6rem] ml-1 text-neutral-800 dark:text-amber-100 cursor-pointer"
     >
       Mahdi Baderloo
     </a>
