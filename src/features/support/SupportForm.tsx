b810c66f0f14ca989f7123d40c983f08c2a7eefc
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-11 15:19:09 +0330

    responsive Support (desktop size)

--- src/features/support/SupportForm.tsx
+++ src/features/support/SupportForm.tsx
@@ -7,26 +7,26 @@ function SupportForm() {
     <form className="lg:w-[90%] lg:overflow-hidden lg:mt-1">
       <h3 className="text-amber-100">How can we help you?</h3>
       <div className="lg:flex lg:gap-2">
-        <div className="flex items-center gap-2 lg:gap-1 bg-amber-100 p-2 rounded-lg mt-2 sm:mt-4 lg:mt-2 lg:w-[49%] lg:p-1 lg:h-10">
-          <BsPersonFill size={40} color="#262626" className="" />
+        <div className="flex items-center gap-2 lg:gap-1 bg-amber-100 p-2 rounded-lg mt-2 sm:mt-4 lg:mt-2 lg:w-[49%] lg:p-1 lg:h-10 xl:h-12">
+          <BsPersonFill size={40} color="#262626" className="x l:w-10" />
           <input
             type="text"
-            className="border-none outline-none text-neutral-800 text-sm lg:text-[0.8rem]"
+            className="border-none outline-none text-neutral-800 text-sm lg:text-[0.8rem] xl:text-[1rem]"
             placeholder="Name"
           />
         </div>
-        <div className="flex items-center gap-2 lg:gap-1 bg-amber-100 p-2 rounded-lg mt-2 lg:mt-2 lg:w-[49%] lg:p-1 lg:h-10">
-          <MdEmail size={40} color="#262626" className="" />
+        <div className="flex items-center gap-2 lg:gap-1 bg-amber-100 p-2 rounded-lg mt-2 lg:mt-2 lg:w-[49%] lg:p-1 lg:h-10 xl:h-12">
+          <MdEmail size={40} color="#262626" className="xl:w-10" />
           <input
             type="text"
-            className="border-none outline-none text-neutral-800 text-sm lg:text-[0.8rem]"
+            className="border-none outline-none text-neutral-800 text-sm lg:text-[0.8rem] xl:text-[1rem]"
             placeholder="Email"
           />
         </div>
       </div>
       <div>
-        <div className="flex items-start gap-2 bg-amber-100 p-2 rounded-lg mt-2 h-45 lg:h-35 lg:mr-0.5">
-          <FaPen size={35} color="#262626" />
+        <div className="flex items-start gap-2 bg-amber-100 p-2 rounded-lg mt-2 h-45 lg:h-35 xl:h-40 lg:mr-0.5">
+          <FaPen size={35} color="#262626" className="" />
           <textarea
             className="border-none outline-none text-neutral-800 text-sm w-full h-full"
             maxLength={200}
@@ -37,9 +37,11 @@ function SupportForm() {
           10 / 200
         </p>
       </div>
-      <button className="bg-amber-200 text-neutral-800 rounded-lg p-2 w-full hover:bg-amber-300 transition-all mt-4 lg:mt-2 lg:cursor-pointer">
-        Submit
-      </button>
+      <div className="xl:flex xl:justify-center xl:w-full">
+        <button className="bg-amber-200 text-neutral-800 rounded-lg p-2 w-full xl:w-[40%] hover:bg-amber-300 transition-all mt-4 lg:mt-2 lg:cursor-pointer">
+          Submit
+        </button>
+      </div>
     </form>
   );
 }
--- src/features/support/SupportLinks.tsx
+++ src/features/support/SupportLinks.tsx
@@ -15,26 +15,30 @@ const links: Link[] = [
   {
     id: 1,
     title: "Telegram",
-    icon: <FaTelegramPlane size={30} color="#262626" />,
+    icon: (
+      <FaTelegramPlane size={30} color="#262626" className="xl:w-6 xl:h-6" />
+    ),
     link: "",
   },
   {
     id: 2,
     title: "Call",
-    icon: <BsTelephoneFill size={30} color="#262626" />,
+    icon: (
+      <BsTelephoneFill size={30} color="#262626" className="xl:w-6 xl:h-6" />
+    ),
     link: "",
   },
   {
     id: 3,
     title: "Email",
-    icon: <MdEmail size={30} color="#262626" />,
+    icon: <MdEmail size={30} color="#262626" className="xl:w-6 xl:h-6" />,
     link: "",
   },
 ];
 
 function SupportLinks() {
   return (
-    <ul className="flex lg:flex-col lg:gap-3 justify-evenly lg:justify-center items-center text-amber-200 mt-4 lg:ml-4">
+    <ul className="flex lg:flex-col xl:flex-row xl:mx-auto lg:gap-3 xl:gap-12 justify-evenly lg:justify-center items-center text-amber-200 mt-4 lg:ml-4">
       {links.map((link) => (
         <li
           key={link.id}
@@ -43,7 +47,7 @@ function SupportLinks() {
           <p className="bg-amber-200 w-fit p-2 sm:p-3 rounded-full hover:bg-amber-300 transition-all">
             {link.icon}
           </p>
-          <span className="text-[0.6rem] sm:text-[0.75rem] lg:text-[0.6rem]">
+          <span className="text-[0.6rem] sm:text-[0.75rem] lg:text-[0.6rem] xl:text-[0.7rem]">
             {link.title}
           </span>
         </li>
