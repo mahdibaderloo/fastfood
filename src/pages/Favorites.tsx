9ef38d97d3a9ec5f3a12d0b7fc2aa1f1b28ca7b9
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-11 15:05:02 +0330

    responsive Favorites (desktop size)

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -14,7 +14,7 @@ function Favorites() {
         showBackButton={true}
         classP="text-4xl dark:text-amber-300"
       />
-      <main className="overflow-y-scroll lg:scrollbar-hide pt-20 h-screen lg:w-[80%] lg:mx-auto lg:p-4 lg:pt-16">
+      <main className="overflow-y-scroll lg:scrollbar-hide pt-20 h-screen lg:w-[80%] xl:w-[1100px] lg:mx-auto lg:p-4 xl:px-2 lg:pt-16">
         <BackButton />
         <ul className="flex items-center justify-start flex-wrap gap-2 p-2 sm:justify-between lg:justify-center sm:mx-auto sm:w-90 lg:w-full lg:mt-4">
           {favorites.map((item) => (
