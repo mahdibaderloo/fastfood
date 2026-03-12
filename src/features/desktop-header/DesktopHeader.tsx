ff3921df4210fde2c9d6747799723b2d55268256
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-03-12 12:18:09 +0330

    responsive Header (large size)

--- src/features/desktop-header/DesktopHeader.tsx
+++ src/features/desktop-header/DesktopHeader.tsx
@@ -14,7 +14,7 @@ function DesktopHeader() {
 
   return (
     <header className="p-2 py-4 fixed right-0 left-0 z-50 bg-amber-300 dark:bg-neutral-800">
-      <div className="w-[80%] xl:w-[1200px] mx-auto flex items-center justify-between">
+      <div className="w-[80%] xl:w-[1200px] 2xl:w-[1420px] mx-auto flex items-center justify-between">
         <div
           className="flex items-center cursor-pointer"
           onClick={() => navigate("/")}
