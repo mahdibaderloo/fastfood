8a1a16d3583178a8534e2f5446ac6c60198159b1
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-05-17 10:51:47 +0330

    Item dark styels

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -48,13 +48,13 @@ function Item() {
           {item?.isFavorite ? (
             <GoHeartFill
               size={30}
-              color="#ffd230c8"
+              className="text-neutral-800 dark:text-[#ffd230c8]"
               // onClick={handleRemoveItemFromFavorites}
             />
           ) : (
             <GoHeart
               size={30}
-              color="#ffd230c8"
+              className="text-neutral-800 dark:text-[#ffd230c8]"
               // onClick={handleAddItemToFavorites}
             />
           )}
@@ -65,27 +65,33 @@ function Item() {
         <div className="bg-neutral-400 w-[100%] h-40 p-2 rounded-b-[50%] scale-180 overflow-hidden">
           <img src={item?.image} alt="item-image" />
         </div>
-        <div className="bg-amber-300 h-full pt-18 px-4 flex flex-col gap-4">
-          <h3 className="text-xl text-neutral-900">{item?.productName}</h3>
+        <div className="bg-amber-300 dark:bg-neutral-800 h-full pt-18 px-4 flex flex-col gap-4">
+          <h3 className="text-xl text-neutral-900 dark:text-amber-300">
+            {item?.productName}
+          </h3>
           <div className="flex items-center gap-2">
-            <FaMoneyBillWave color="#404040" className="text-sm" />
-            <p className="text-neutral-700 text-lg">${item?.price}</p>
+            <FaMoneyBillWave className="text-sm text-neutral-700 dark:text-amber-100" />
+            <p className="text-neutral-700 dark:text-amber-100 text-lg">
+              ${item?.price}
+            </p>
           </div>
-          <p className="text-justify text-neutral-700 text-sm">
+          <p className="text-justify text-neutral-700 dark:text-amber-200 text-sm">
             {item?.description}
           </p>
           <div className="flex justify-center">
-            <button className="flex justify-center items-center gap-2 dark:bg-neutral-800 bg-amber-300 dark:text-amber-300 text-neutral-800 text-sm 2xl:text-xl p-2 sm:p-3 rounded-lg sm:rounded-xl w-full lg:w-[40%] 2xl:w-[50%] 2xl:h-14 mt-6 shadow lg:cursor-pointer lg:hover:bg-neutral-950 lg:dark:hover:bg-amber-400 transition-all">
-              <CiShoppingCart className="text-2xl text-neutral-800 dark:text-amber-300" />{" "}
+            <button className="flex justify-center items-center gap-2 bg-neutral-800 dark:bg-amber-300 text-amber-300 dark:text-neutral-800 text-sm 2xl:text-xl p-2 sm:p-3 rounded-lg sm:rounded-xl w-full lg:w-[40%] 2xl:w-[50%] 2xl:h-14 mt-6 shadow lg:cursor-pointer lg:hover:bg-neutral-950 lg:dark:hover:bg-amber-400 transition-all">
+              <CiShoppingCart className="text-2xl dark:text-neutral-800 text-amber-300" />{" "}
               Add to cart
             </button>
           </div>
           <div className="pt-4">
             <div className="flex items-center gap-1">
-              <FaComments className="text-sm" color="#262626" />
-              <p className="text-sm text-neutral-800">Comments</p>
+              <FaComments className="text-sm text-neutral-800 dark:text-amber-200" />
+              <p className="text-sm text-neutral-800 dark:text-amber-200">
+                Comments
+              </p>
             </div>
-            <ul className="bg-amber-300 w-full pt-2 pb-4 flex items-center gap-2 overflow-x-scroll">
+            <ul className="w-full pt-2 pb-4 flex items-center gap-2 overflow-x-scroll">
               {item?.comments.map((comment, index) => (
                 <li key={index} className="bg-neutral-400 p-2 rounded-xl">
                   <p className="text-sm text-neutral-900">owner</p>
