f763558b14681c467c38816c2523a944d0ef8b41
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-05-18 21:20:33 +0330

    add ItemContent

---
+++ src/features/item/ItemContent.tsx
@@ -0,0 +1,37 @@
+import { FaMoneyBillWave } from "react-icons/fa6";
+
+interface Item {
+  item: {
+    id: number;
+    productName: string;
+    price: number;
+    category: string;
+    description: string;
+    image: string;
+    comments: string[];
+    score: number;
+  };
+}
+
+export default function ItemContent({ item }: Item) {
+  if (!item) {
+    return <p>Food not found</p>;
+  }
+
+  return (
+    <>
+      <h3 className="text-xl text-neutral-900 dark:text-amber-300">
+        {item?.productName}
+      </h3>
+      <div className="flex items-center gap-2">
+        <FaMoneyBillWave className="text-sm text-neutral-700 dark:text-amber-100" />
+        <p className="text-neutral-700 dark:text-amber-100 text-lg">
+          ${item?.price}
+        </p>
+      </div>
+      <p className="text-justify text-neutral-700 dark:text-amber-200 text-sm">
+        {item?.description}
+      </p>
+    </>
+  );
+}
--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -10,8 +10,9 @@ import Loading from "../components/Loading";
 import Header from "../components/Header";
 import { GoHeart, GoHeartFill } from "react-icons/go";
 import BackButton from "../components/BackButton";
-import { FaComments, FaMoneyBillWave } from "react-icons/fa6";
-import { CiShoppingBasket, CiShoppingCart } from "react-icons/ci";
+import { FaComments } from "react-icons/fa6";
+import { CiShoppingCart } from "react-icons/ci";
+import ItemContent from "../features/item/ItemContent";
 
 function Item() {
   const { data: items, isLoading } = useFoods();
@@ -61,46 +62,17 @@ function Item() {
         </div>
       </Header>
       <main className="min-h-screen overflow-y-scroll overflow-x-hidden pt-15 flex flex-col items-center">
-        <BackButton />
+        {/* <BackButton /> */}
         <div className="bg-neutral-400 w-[100%] h-40 p-2 rounded-b-[50%] scale-180 overflow-hidden">
           <img src={item?.image} alt="item-image" />
         </div>
         <div className="bg-amber-300 dark:bg-neutral-800 h-full pt-18 px-4 flex flex-col gap-4">
-          <h3 className="text-xl text-neutral-900 dark:text-amber-300">
-            {item?.productName}
-          </h3>
-          <div className="flex items-center gap-2">
-            <FaMoneyBillWave className="text-sm text-neutral-700 dark:text-amber-100" />
-            <p className="text-neutral-700 dark:text-amber-100 text-lg">
-              ${item?.price}
-            </p>
-          </div>
-          <p className="text-justify text-neutral-700 dark:text-amber-200 text-sm">
-            {item?.description}
-          </p>
+          <ItemContent item={item} />
           <div className="flex justify-center">
             <button className="flex justify-center items-center gap-2 bg-neutral-800 dark:bg-amber-300 text-amber-300 dark:text-neutral-800 text-sm 2xl:text-xl p-2 sm:p-3 rounded-lg sm:rounded-xl w-full lg:w-[40%] 2xl:w-[50%] 2xl:h-14 mt-6 shadow lg:cursor-pointer lg:hover:bg-neutral-950 lg:dark:hover:bg-amber-400 transition-all">
               <CiShoppingCart className="text-2xl dark:text-neutral-800 text-amber-300" />{" "}
               Add to cart
             </button>
-          </div>
-          <div className="pt-4">
-            <div className="flex items-center gap-1">
-              <FaComments className="text-sm text-neutral-800 dark:text-amber-200" />
-              <p className="text-sm text-neutral-800 dark:text-amber-200">
-                Comments
-              </p>
-            </div>
-            <ul className="w-full pt-2 pb-4 flex items-center gap-2 overflow-x-scroll">
-              {item?.comments.map((comment, index) => (
-                <li key={index} className="bg-neutral-400 p-2 rounded-xl">
-                  <p className="text-sm text-neutral-900">owner</p>
-                  <span className="text-[14px] text-neutral-800">
-                    {comment}
-                  </span>
-                </li>
-              ))}
-            </ul>
           </div>
         </div>
       </main>
