f7bce1f4709e607566bacf50bfc5b19cd185fe4c
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-05-19 20:27:43 +0330

    use Button for Address page

--- src/features/address/AddNewAddressButton.tsx
+++ src/features/address/AddNewAddressButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useModalStore } from "../../store/modalStore";
 import AddressForm from "./AddressForm";
+import Button from "../../components/Button";
 
 function AddNewAddressButton() {
   const { open } = useModalStore();
@@ -11,12 +12,14 @@ function AddNewAddressButton() {
   }
   return (
     <div className="sm:w-80 sm:mx-auto">
-      <button
-        className="bg-amber-200 text-neutral-800 w-full rounded-lg sm:rounded-xl p-2 mt-10 lg:cursor-pointer lg:hover:bg-amber-100 transition-all lg:shadow"
-        onClick={handleAddAddress}
-      >
-        Add New Address
-      </button>
+      <Button
+        text="Add New Address"
+        bg="bg-amber-200"
+        darkBg="bg-amber-200"
+        textColor="text-neutral-800"
+        textDarkColor="text-neutral-800"
+        onclick={handleAddAddress}
+      ></Button>
     </div>
   );
 }
