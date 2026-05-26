98918117b4f44d70b3df992c75dd1c96617c80f5
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2026-05-26 20:22:20 +0330

    Login styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,10 +11,10 @@ export default function Button({
   onclick,
 }: ButtonProps) {
   return (
-    <div className={divStyles ? divStyles : "flex justify-center"}>
+    <div className={divStyles ? divStyles : "flex justify-center w-full"}>
       <button
         onClick={onclick}
-        className={`flex items-center justify-center gap-2 ${bg} dark:${darkBg} ${textColor} dark:${textDarkColor} 2xl:text-xl p-2 sm:p-3 rounded-lg sm:rounded-xl w-full lg:w-[40%] 2xl:w-[50%] 2xl:h-14 mt-6 shadow lg:cursor-pointer lg:hover:bg-neutral-950 lg:dark:hover:bg-amber-400 transition-all`}
+        className={`flex items-center justify-center gap-2 ${bg} dark:${darkBg} ${textColor} dark:${textDarkColor} 2xl:text-xl p-2 sm:p-3 rounded-lg sm:rounded-xl w-full 2xl:h-14 mt-6 shadow lg:cursor-pointer lg:hover:bg-neutral-950 lg:dark:hover:bg-amber-400 transition-all`}
       >
         {children}
         {text}
